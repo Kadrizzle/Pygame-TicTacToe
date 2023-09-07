10f64d3ffa1a51f4baebda13de3c75bdc2adc4eb
Kade Lane <lanekade8@gmail.com>
2023-09-07 16:26:47 -0500

    changed open_spots array to 1s and 0s

--- tictactoe_pygame.py
+++ tictactoe_pygame.py
@@ -2,6 +2,9 @@
 #pygame.mouse.get_pressed() & pygame.mouse.get_pos() can be used to put the 'X' and 'O' on the board
 #Call pygame.event.get() before the pygame.mouse.get_pressed() in order for it to work
 
+#After one of the if statements in the main loop I need to call a function that makes the computer play. 
+#Once it plays I need to put a "1" in the "open_spots" array correlating with the spot the computer played
+
 #When drawing X or O I need to keep track of the turns so I know which one to draw
 import pygame
 
@@ -21,7 +24,7 @@ sixth_slot = pygame.draw.rect(screen, (255,255,255), (275, 174, 74, 99))
 seventh_slot = pygame.draw.rect(screen, (255,255,255), (100, 274, 74, 74))
 eigth_slot = pygame.draw.rect(screen, (255,255,255), (175, 274, 99, 74))
 ninth_slot = pygame.draw.rect(screen, (255,255,255), (275, 274, 74, 74))
-open_spots = [True, True, True, True, True, True, True, True, True]
+open_spots = [0, 0, 0, 0, 0, 0, 0, 0, 0]
 
 
 #This function will return true or false for the LEFT mouse click
@@ -39,7 +42,13 @@ def draw_board():
     #top line
     pygame.draw.line(screen, pygame.Color(0,0,0), ((screen_x * 0) + 100, (screen_y / 2) - 47), (screen_x - 95, (screen_y / 2) - 47), width = 6)   
     #bottom line
-    pygame.draw.line(screen, pygame.Color(0,0,0), ((screen_x * 0) + 100, (screen_y / 2) + 45), (screen_x - 95, (screen_y / 2) + 45), width = 6)    
+    pygame.draw.line(screen, pygame.Color(0,0,0), ((screen_x * 0) + 100, (screen_y / 2) + 45), (screen_x - 95, (screen_y / 2) + 45), width = 6) 
+    
+#def computer_play():
+    
+   # for x in open_spots:
+        
+       
     
 pygame.init()
 screen.fill("white")
@@ -56,50 +65,50 @@ while running:
     if get_mouse_click() == True:
         mouse_x, mouse_y = pygame.mouse.get_pos() 
         
-        if first_slot.collidepoint(mouse_x, mouse_y) and open_spots[0] == True:
+        if first_slot.collidepoint(mouse_x, mouse_y) and open_spots[0] == 0:
             pygame.draw.line(screen, pygame.Color(64, 61, 88), (105, 105), (170, 170), width = 6)
             pygame.draw.line(screen, pygame.Color(64,61,88), (170, 105), (105, (screen_y /2) - 55), width = 6)
-            open_spots[0] = False
+            open_spots[0] = 1
             
-        if second_slot.collidepoint(mouse_x, mouse_y) and open_spots[1] == True:
+        if second_slot.collidepoint(mouse_x, mouse_y) and open_spots[1] == 0:
             pygame.draw.line(screen, pygame.Color(64, 61, 88), (195,105), (260, 170), width = 6)
             pygame.draw.line(screen, pygame.Color(64,61,88), (260, 105), (195, 170), width = 6) 
-            open_spots[1] = False
+            open_spots[1] = 1
                
-        if third_slot.collidepoint(mouse_x, mouse_y) and open_spots[2] == True:
+        if third_slot.collidepoint(mouse_x, mouse_y) and open_spots[2] == 0:
             pygame.draw.line(screen, pygame.Color(64, 61, 88), (285,105), (350,170), width = 6)
             pygame.draw.line(screen, pygame.Color(64,61,88), (285,170),(350,105), width = 6) 
-            open_spots[2] = False
+            open_spots[2] = 1
                 
-        if fourth_slot.collidepoint(mouse_x, mouse_y) and open_spots[3] == True:
+        if fourth_slot.collidepoint(mouse_x, mouse_y) and open_spots[3] == 0:
             pygame.draw.line(screen, pygame.Color(64, 61, 88), (105,190), (170, 255), width = 6)
             pygame.draw.line(screen, pygame.Color(64, 61, 88), (170,190), (105, 255), width = 6)
-            open_spots[3] = False
+            open_spots[3] = 1
     
-        if fifth_slot.collidepoint(mouse_x, mouse_y) and open_spots[4] == True:
+        if fifth_slot.collidepoint(mouse_x, mouse_y) and open_spots[4] == 0:
             pygame.draw.line(screen, pygame.Color(64, 61, 88), (195,190), (260, 255), width = 6)
             pygame.draw.line(screen, pygame.Color(64, 61, 88), (260,190), (195, 255), width = 6)
-            open_spots[4] = False 
+            open_spots[4] = 1 
     
-        if sixth_slot.collidepoint(mouse_x, mouse_y) and open_spots[5] == True:
+        if sixth_slot.collidepoint(mouse_x, mouse_y) and open_spots[5] == 0:
             pygame.draw.line(screen, pygame.Color(64, 61, 88), (285,190), (350,255), width = 6)
             pygame.draw.line(screen, pygame.Color(64,61,88), (285,255),(350,190), width = 6)
-            open_spots[5] = False
+            open_spots[5] = 1
                
-        if seventh_slot.collidepoint(mouse_x, mouse_y) and open_spots[6] == True:
+        if seventh_slot.collidepoint(mouse_x, mouse_y) and open_spots[6] == 0:
             pygame.draw.line(screen, pygame.Color(64, 61, 88), (105,280), (170, 345), width = 6)
             pygame.draw.line(screen, pygame.Color(64, 61, 88), (170,280), (105, 345), width = 6)
-            open_spots[6] = False
+            open_spots[6] = 1
             
-        if eigth_slot.collidepoint(mouse_x, mouse_y) and open_spots[7] == True:
+        if eigth_slot.collidepoint(mouse_x, mouse_y) and open_spots[7] == 0:
             pygame.draw.line(screen, pygame.Color(64, 61, 88), (195,280), (260, 345), width = 6)
             pygame.draw.line(screen, pygame.Color(64, 61, 88), (260,280), (195, 345), width = 6) 
-            open_spots[7] = False
+            open_spots[7] = 1
             
-        if ninth_slot.collidepoint(mouse_x, mouse_y) and open_spots[8] == True:
+        if ninth_slot.collidepoint(mouse_x, mouse_y) and open_spots[8] == 0:
             pygame.draw.line(screen, pygame.Color(64, 61, 88), (285,280), (350, 345), width = 6)
             pygame.draw.line(screen, pygame.Color(64, 61, 88), (285,345), (350, 280), width = 6)
-            open_spots[8] = False
+            open_spots[8] = 1
             
     # #first
     # pygame.draw.line(screen, pygame.Color(64, 61, 88), (105, 105), ((screen_x / 2) - 55, (screen_y / 2) - 55), width = 6)
